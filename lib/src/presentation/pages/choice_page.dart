4347b4002d5cb6ea919cd78102aad0d6d44ddcb3
liudmylatymoshenko <timlv7777@gmail.com>
2023-10-10 13:32:11 +0300

    choice_page - Added navigation based on state changes

--- lib/src/presentation/pages/choice_page.dart
+++ lib/src/presentation/pages/choice_page.dart
@@ -1,5 +1,7 @@
 import 'package:flora_test/src/config/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../utils/app_strings.dart';
 import '../bloc/choice/choice_bloc.dart';
 import '../widgets/choice_button_widget.dart';
@@ -13,37 +15,48 @@ class ChoicePage extends StatelessWidget {
     return Scaffold(
       body: Stack(alignment: Alignment.center, children: [
         SvgImageWidget(image: theme.images.choiceBackground),
-        Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.28,
-            ),
-            ChoiceButtonWidget(
-              title: AppStrings.trackMyPeriod,
-              subtitle: AppStrings.contraceptionAndWellBeing,
-              onTap: (){
-                choiceBloc.add(TrackPeriodChoiceEvent());
-                navigateToDateOfBirthPage(context);
-              },
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.08,
-            ),
-            ChoiceButtonWidget(
-              title: AppStrings.getPregnant,
-              subtitle: AppStrings.learnAboutReproductiveHealth,
-              onTap: () {
-                choiceBloc.add(GetPregnantChoiceEvent());
-                navigateToDateOfBirthPage(context);
-              },
-            ),
-          ],
-        )
+        BlocBuilder<ChoiceBloc, ChoiceState>(
+            bloc: choiceBloc,
+            builder: (context, state) {
+              checkState(state, context);
+              return Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.28,
+                  ),
+                  ChoiceButtonWidget(
+                    title: AppStrings.trackMyPeriod,
+                    subtitle: AppStrings.contraceptionAndWellBeing,
+                    onTap: () {
+                      choiceBloc.add(TrackPeriodChoiceEvent());
+                    },
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
+                  ChoiceButtonWidget(
+                    title: AppStrings.getPregnant,
+                    subtitle: AppStrings.learnAboutReproductiveHealth,
+                    onTap: () {
+                      choiceBloc.add(GetPregnantChoiceEvent());
+                    },
+                  ),
+                ],
+              );
+            })
       ]),
     );
   }
 
+  void checkState(ChoiceState state, BuildContext context) {
+    if (state != ChoiceState.empty) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        navigateToDateOfBirthPage(context);
+      });
+    }
+  }
+
   void navigateToDateOfBirthPage(BuildContext context) {
-    appNavigator.goToDateOfBirth(context);
+      appNavigator.goToDateOfBirth(context);
   }
 }
